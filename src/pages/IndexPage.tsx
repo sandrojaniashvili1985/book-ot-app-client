2486445ebabe457149f9dc80eafa7f9a5ec13598
sandro janiashvili <janiashvili.aleksandre@gmail.com>
2024-04-11 23:08:07 +0300

    dfsdg

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -9,7 +9,7 @@ const IndexPage = () => {
 
   useEffect(() => {
     const AllHotelsData = async () => {
-      const { data } = await axios.get("https://book-it-app-six.vercel.app");
+      const { data } = await axios.get("/api/hotels/");
       setHotels(data);
     };
     AllHotelsData();
